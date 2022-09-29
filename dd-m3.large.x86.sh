3bb463977dde2a9793fc754605f495a7857ed51d
Naji Forest Pride <npride@equinix.com>
2022-09-29 12:50:13 -0400

    Create dd-m3.large.x86.sh
    Disk discovery script for the Equinix Metal m3.large.x86 server plan.

---
+++ dd-m3.large.x86.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ssd=()
+nvme=()
+all=$(lsblk -lnpdb -I 8,259 -x SIZE -o NAME\)
+
+while read path
+do
+    mountpoint=$(lsblk -ln -o MOUNTPOINT $path)
+    
+    if [[ -n $mountpoint ]]
+    then
+        continue
+    elif [[ $path =~ "nvme" ]]
+    then
+        nvme+=("$path")
+    else
+        ssd+=("$path")
+    fi
+done <<< $all
+
+iter=0
+for disk in ${ssd[@]}
+do
+    ln -s $disk "/dev/ssd-drive-$iter"
+    ((++iter))
+done
+
+iter=0
+for disk in ${nvme[@]}
+do
+    ln -s $disk "/dev/nvme-drive-$iter"
+    ((++iter))
+done
