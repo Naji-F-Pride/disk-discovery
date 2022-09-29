8062437c3c1705be1556a0058bbb27e90e395dc0
Naji Forest Pride <npride@equinix.com>
2022-09-29 14:34:07 -0400

    Remove non breaking spaces
    Non breaking space characters were causing problems with bash execution of the script.

--- dd-m3.large.x86.sh
+++ dd-m3.large.x86.sh
@@ -6,29 +6,29 @@ all=$(lsblk -lnpdb -I 8,259 -x SIZE -o NAME)
 
 while read path
 do
-    mountpoint=$(lsblk -ln -o MOUNTPOINT $path)
-    
-    if [[ -n $mountpoint ]]
-    then
-        continue
-    elif [[ $path =~ "nvme" ]]
-    then
-        nvme+=("$path")
-    else
-        ssd+=("$path")
-    fi
+    mountpoint=$(lsblk -ln -o MOUNTPOINT $path)
+
+    if [[ -n $mountpoint ]]
+    then
+        continue
+    elif [[ $path =~ "nvme" ]]
+    then
+        nvme+=("$path")
+    else
+        ssd+=("$path")
+    fi
 done <<< $all
 
 iter=0
 for disk in ${ssd[@]}
 do
-    ln -s $disk "/dev/ssd-drive-$iter"
-    ((++iter))
+    ln -s $disk "/dev/ssd-drive-$iter"
+    ((++iter))
 done
 
 iter=0
 for disk in ${nvme[@]}
 do
-    ln -s $disk "/dev/nvme-drive-$iter"
-    ((++iter))
+    ln -s $disk "/dev/nvme-drive-$iter"
+    ((++iter))
 done
